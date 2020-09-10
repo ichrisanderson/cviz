66e7ad0438220307432141ff735f25b40f2bb37d
Chris Anderson <cjanderson2@hotmail.com>
2020-09-10 17:35:30 +0100

    Add pending intent

--- app/src/main/java/com/chrisa/covid19/core/data/synchronisation/SyncNotification.kt
+++ app/src/main/java/com/chrisa/covid19/core/data/synchronisation/SyncNotification.kt
@@ -18,9 +18,12 @@ package com.chrisa.covid19.core.data.synchronisation
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import com.chrisa.covid19.MainActivity
 import com.chrisa.covid19.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -40,6 +43,10 @@ class SyncNotificationImpl @Inject constructor(
             context.getString(R.string.sync_notification_name),
             context.getString(R.string.sync_notification_success_description)
         )
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
@@ -47,6 +54,7 @@ class SyncNotificationImpl @Inject constructor(
             .setContentText(context.getString(R.string.sync_notification_success_content_text))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setAutoCancel(false)
+            .setContentIntent(pendingIntent)
             .build()
 
         // Send notification
